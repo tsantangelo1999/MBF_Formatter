5d8b6b520ef33776d326f279aae037aa8d7ef469
tsantangelo1999 <tommys2056@gmail.com>
2018-05-22 16:03:33 -0400

    it almost worked and thats whats important

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-	    File config = new File("configTest.txt");
+        File config = new File("configTest.txt");
         File data = new File("data.txt");
         //data is currently experimental and subject to change
         FileWriter fw = new FileWriter("MBF.txt");
@@ -63,6 +63,52 @@ public class Main
         System.out.println(print(format));
         sc.close();
         Scanner sc2 = new Scanner(data);
+        ArrayList<String[]> dataParams = new ArrayList<>();
+        while(sc2.hasNextLine())
+        {
+            dataParams.add(sc2.nextLine().split("\t"));
+        }
+        sc2.close();
+        for(ArrayList<Object[]> a : format)
+        {
+            for(Object[] b : a)
+            {
+                int index;
+                try
+                {
+                    index = find((String)b[2], dataParams);
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Field not found in data: " + e.getMessage());
+                    return;
+                }
+                if(dataParams.get(index)[1].length() > ((int)b[1] - (int)b[0] + 1))
+                {
+                    System.out.println("Data for " + dataParams.get(index)[0] + " is too long. Length " + dataParams.get(index)[1].length() + " > " + ((int)b[1] - (int)b[0] + 1));
+                    return;
+                }
+                pw.print(dataParams.get(index)[1]);
+                for(int i = dataParams.get(index)[1].length(); i < ((int)b[1] - (int)b[0] + 1); i++)
+                {
+                    pw.print(" ");
+                }
+            }
+        }
+        pw.close();
+        fw.close();
+    }
+
+    public static int find(String target, ArrayList<String[]> data) throws Exception
+    {
+        for(int i = 0; i < data.size(); i++)
+        {
+            if(data.get(i)[0] == target)
+            {
+                return i;
+            }
+        }
+        throw new Exception(target);
     }
 
     public static String print(ArrayList<ArrayList<Object[]>> a)
